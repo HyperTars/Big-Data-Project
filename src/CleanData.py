e40fdc70e46a2616458334f79e902d83a3064a8f
hypertars <hypertars@gmail.com>
2020-04-22 19:52:15 -0400

    trans Yahoo data format to NASDAQ data format

--- src/CleanData.py
+++ src/CleanData.py
@@ -25,6 +25,30 @@ def TransToUSDBase():
 
 
 ########################################
+# Trans Yahoo Data Format to NASDAQ Data Format
+def TransYahooToNASDAQ():
+    path_index = '../data/market/Index/'
+    file_path = []
+    file_path.append(path_index + 'CBOE_VolatilityIndex.csv')
+    file_path.append(path_index + 'DowJones_IndustrialAvg.csv')
+    file_path.append(path_index + 'NASDAQ_100_Index.csv')
+    file_path.append(path_index + 'Russell_2000.csv')
+    for path in file_path:
+        df = pd.read_csv(path)
+        # remove redundant column 'Adj Close'
+        df = df.drop('Adj Close', axis=1, errors='ignore')
+        # rename Close to Close/Last and put to second place
+        df_close = df.Close
+        df = df.drop('Close', axis=1, errors='ignore')
+        df.insert(1, 'Close/Last', df_close)
+        # reorder in time
+        df['Date'] = pd.to_datetime(df['Date'])
+        df.sort_values('Date', inplace=True, ascending=False)
+        # save
+        df.to_csv(path, index=False, header=True)
+
+
+########################################
 # Clean Data From NASDAQ (Excepet All Stock Data)
 def CleanDataFromNASDAQ():
     file_idx = 'NasdaqSource.txt'
