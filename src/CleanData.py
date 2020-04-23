a44fffb18a1a66b1e97e4d00b155784ac65fdba0
hypertars <hypertars@gmail.com>
2020-04-22 20:46:43 -0400

    update CleanData.py

--- src/CleanData.py
+++ src/CleanData.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
-
+import xlrd
 
 def removeExist(path):
     if(os.path.exists(path)):
@@ -63,7 +63,7 @@ def CleanDataFromNASDAQ():
 
     for path in file_path.values():
         df = pd.read_csv(path)
-        # format column name
+        # format column name, delete redundant whitespace in column names
         df.columns = [x.strip() for x in df.columns.values if x.strip() != '']
         # remove redundant column 'Volume'
         df = df.drop('Volume', axis=1, errors='ignore')
@@ -76,13 +76,46 @@ def CleanDataFromNASDAQ():
 ########################################
 # Clean Data From Other Source
 def CleanOtherSources():
-    CrudeOil_WTI_MacroTrends = '../data/market/Commodities/Energies/CrudeOil_WTI_macrotrends.csv'
-    Finance_Sector_Related_Policy_Responses = '../data/general/FinanceSectorRelatedPolicyResponses.xlsx'
-    Industrial_Production_Index = '../data/general/IndustrialProductionIndex.csv'
-    Initial_Jobless_Claims = '../data/employment/InitialJoblessClaims.csv'
-    Unemployment_Rate = '../data/employment/UnemploymentRate.csv'
+    file_CrudeOil_WTI_MacroTrends = '../data/market/Commodities/Energies/CrudeOil_WTI_macrotrends.csv'
+    file_Finance_Sector_Related_Policy_Responses = '../data/general/FinanceSectorRelatedPolicyResponses.xlsx'
+    file_Industrial_Production_Index = '../data/general/IndustrialProductionIndex.csv'
+    file_Initial_Jobless_Claims = '../data/employment/InitialJoblessClaims.csv'
+    file_Unemployment_Rate = '../data/employment/UnemploymentRate.csv'
 
+    CWM = pd.read_csv(file_CrudeOil_WTI_MacroTrends)
+    # reorder in time
+    CWM['date'] = pd.to_datetime(CWM['date'])
+    CWM.sort_values('date', inplace=True, ascending=False)
+    CWM.to_csv(file_CrudeOil_WTI_MacroTrends, index=False, header=True)
 
-TransToUSDBase()
-CleanDataFromNASDAQ()
-# CleanOtherSources()
+    FSRPR = pd.read_excel(file_Finance_Sector_Related_Policy_Responses, sheet_name='Raw data')
+    # drop redundant columns
+    FSRPR = FSRPR.drop('Iso 3 Code', axis=1, errors='ignore')
+    FSRPR = FSRPR.drop('Entry date', axis=1, errors='ignore')
+    # reorder
+    FSRPR_Country = FSRPR.Country
+    FSRPR = FSRPR.drop('Country', axis=1, errors='ignore')
+    FSRPR.insert(0, 'Country', FSRPR_Country)
+    FSRPR.to_csv(file_Finance_Sector_Related_Policy_Responses[:-4] + 'csv', index=False, header=True)
+
+    IPI = pd.read_csv(file_Industrial_Production_Index)
+    # reorder in time
+    IPI['DATE'] = pd.to_datetime(IPI['DATE'])
+    IPI.sort_values('DATE', inplace=True, ascending=False)
+    IPI.to_csv(file_Industrial_Production_Index, index=False, header=True)
+
+    IJC = pd.read_csv(file_Initial_Jobless_Claims)
+    IJC['DATE'] = pd.to_datetime(IJC['DATE'])
+    IJC.sort_values('DATE', inplace=True, ascending=False)
+    IJC.to_csv(file_Initial_Jobless_Claims, index=False, header=True)
+
+    UR = pd.read_csv(file_Unemployment_Rate)
+    # reorder in time
+    UR['DATE'] = pd.to_datetime(UR['DATE'])
+    UR.sort_values('DATE', inplace=True, ascending=False)
+    UR.to_csv(file_Unemployment_Rate, index=False, header=True)
+
+#TransToUSDBase()
+#TransYahooToNASDAQ()
+#CleanDataFromNASDAQ()
+CleanOtherSources()
